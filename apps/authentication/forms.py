bed42fb7a4a0db039e955f7623dd7aafd56a2aea
naayifmuhammad <muhdnaayif@gmail.com>
2024-03-25 00:48:07 +0530

    User registration and login working

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -27,19 +27,21 @@ class LoginForm(forms.Form):
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
+        max_length=100,
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
+                "placeholder": "Business Name",
                 "class": "form-control"
             }
         ))
     email = forms.EmailField(
         widget=forms.EmailInput(
             attrs={
-                "placeholder": "Email",
+                "placeholder": "Business Email",
                 "class": "form-control"
             }
         ))
+
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
@@ -55,6 +57,23 @@ class SignUpForm(UserCreationForm):
             }
         ))
 
+    business_phone = forms.CharField(
+        max_length=15,
+        widget=forms.NumberInput(
+            attrs={
+                "placeholder": "Business Phone",
+                "class": "form-control"
+            }
+        ))
+    business_address = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Business Address",
+                "class": "form-control"
+            }
+        ))
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'business_phone', 'business_address')
